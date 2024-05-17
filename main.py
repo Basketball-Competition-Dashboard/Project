896e5238619b8c4417a78779a287122ccc6cc8e3
cloudy9982 <108703022@g.nccu.edu.tw>
2024-05-17 14:29:03 +0800

    add project

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
 from flask_moment import Moment
 from datetime import datetime
-from view.purchase import branch_app
-from view.member import member_app
-from view.menu import menu_app
-from view.ingredient import ingredient_app
+
 
 
 
 app = Flask(__name__)
 app.secret_key="coffee.db"
 moment = Moment(app)
-app.register_blueprint(branch_app)
-app.register_blueprint(member_app)
-app.register_blueprint(menu_app)
-app.register_blueprint(ingredient_app)
+# app.register_blueprint(branch_app)
+# app.register_blueprint(member_app)
+# app.register_blueprint(menu_app)
+# app.register_blueprint(ingredient_app)
 
 
 @app.route('/')
@@ -25,4 +22,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0",port = 3000)
